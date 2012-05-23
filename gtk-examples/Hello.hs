64a41e38ca79068889929c6ca6db2f48be078292
Justin Ethier <justin.ethier@gmail.com>
2012-05-22 22:41:02 -0400

    Initial file

---
+++ gtk-examples/Hello.hs
@@ -0,0 +1,11 @@
+-- Examples from http://www.haskell.org/haskellwiki/Gtk2Hs/Tutorials/Intro
+import Graphics.UI.Gtk
+import Control.Monad.Trans(liftIO)
+
+main = do
+    initGUI
+    window <- windowNew
+    window `on` deleteEvent $ liftIO mainQuit >> return False
+    -- ^ IE, on window deleteEvent ...
+    widgetShowAll window
+    mainGUI
